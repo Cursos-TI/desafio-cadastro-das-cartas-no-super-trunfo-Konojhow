d438a05da7c5f71c8dba81e5a2e46d0204223541
Konojhow <jhow.kai@hotmail.com>
2025-04-28 13:13:28 +0000

    Inclusão Densidade e PIB per Capita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,11 +6,14 @@ struct Carta {
     float area;
     float pib;
     int numPontosTuristicos;
+    float densidade;
+    float pibper;
+
 };
 
 int main() {
 
-    printf("Desafio Super Trunfo! \n");
+    printf("# Desafio Super Trunfo! # \n");
     // Declaração de duas variáveis do tipo Carta para armazenar os dados das cidades
     struct Carta cidade1;
     struct Carta cidade2;
@@ -25,7 +28,18 @@ int main() {
     scanf("%f", &cidade1.pib);
     printf("Número de pontos turísticos: ");
     scanf("%d", &cidade1.numPontosTuristicos);
+    //Inclusão da Densidade e Per Capita Cidade1;
+    printf("Densidade Populacional: (Hab./Km²) ");
+    scanf("%f", &cidade1.densidade);
+    printf("PIB per Capita: ");
+    scanf("%f", &cidade1.pibper);
+
+
+
     printf("\n");
+    
+    
+
 
     // Cadastro da segunda cidade
     printf("Cadastro da Segunda Cidade:\n");
@@ -37,7 +51,14 @@ int main() {
     scanf("%f", &cidade2.pib);
     printf("Número de pontos turísticos: ");
     scanf("%d", &cidade2.numPontosTuristicos);
+    //Inclusão da Densidade e Per Capita Cidade2;
+    printf("Densidade Populacional (Hab./Km²): ");
+    scanf("%f", &cidade2.densidade);
+    printf("PIB per Capita: ");
+    scanf("%f", &cidade2.pibper);
     printf("\n");
+    
+    
 
     // Exibição dos dados da primeira cidade
     printf("Dados da Primeira Cidade:\n");
@@ -45,6 +66,13 @@ int main() {
     printf("Área: %.2f km²\n", cidade1.area);
     printf("PIB: %.2f milhões de R$\n", cidade1.pib);
     printf("Número de pontos turísticos: %d\n", cidade1.numPontosTuristicos);
+    // Inclusão da Densidade e PIB Per Capita Cidade 1
+    printf("Desnsidade Populacional : %.2f (Hab/km²)\n", cidade1.densidade);
+    printf("PIB per Capita: %.2f\n", cidade1.pibper);
+
+
+
+
     printf("\n");
 
     // Exibição dos dados da segunda cidade
@@ -53,7 +81,13 @@ int main() {
     printf("Área: %.2f km²\n", cidade2.area);
     printf("PIB: %.2f milhões de R$\n", cidade2.pib);
     printf("Número de pontos turísticos: %d\n", cidade2.numPontosTuristicos);
-    printf("\n");
+    // Inclusão da Densidade e PIB Per Capita Cidade 1
+    printf("Desnsidade Populacional : %.2f (Hab/km²)\n", cidade2.densidade);
+    printf("PIB per Capita: %.2f\n", cidade2.pibper); 
+
+
+
+    printf("\n"); 
 
     return 0;
 }
